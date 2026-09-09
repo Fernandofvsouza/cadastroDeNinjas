c9d076d59301e62aa546bc89749ae75452446c05
Fernando souza <fernandofvsouza@icloud.com>
2026-09-09 14:55:59 +0100

    feat: Coloca o lombok na classe MissoesModel e NinjaModel

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.cadastroDeNinjas.Missoes.Model;
 
 import dev.java10x.cadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table( name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -19,43 +25,7 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
 
-    public MissoesModel(Long id, String nome, String descricao) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-    }
 
-    public MissoesModel(Long id, String nome, String descricao, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.ninjas = ninjas;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.java10x.cadastroDeNinjas.Ninjas.Model;
 
 import dev.java10x.cadastroDeNinjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table( name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -27,52 +33,5 @@ public class NinjaModel {
 
 
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel(String nome, String email, int idade, MissoesModel missoes) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.missoes = missoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
 }
